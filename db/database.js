d5dfa83b77e10587902db8053c9bd2dc15a78d2c
David Daly <dalyd14@gmail.com>
2021-01-25 17:27:25 -0500

    added files

--- db/database.js
+++ db/database.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 const db = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: 'dew2you.',
-    database: 'companyDB'
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
 })
 
 module.exports = db
